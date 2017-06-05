500a15a8226bb465d1219ceecb566e0f991c6cda
StutiSri <stutisrivastv@gmail.com>
2017-06-05 23:44:06 +0530

    Added TestCase - shouldTurnRoverAtCommandR. Source Code added to passing test case.

--- src/test/org/tw/marsrover/MarsRoverTest.java
+++ src/test/org/tw/marsrover/MarsRoverTest.java
@@ -13,4 +13,12 @@ public class MarsRoverTest {
         TestRover expected = new TestRover(new Coordinates(1,2), Orientation.N);
         assertEquals(expected, rover);
     }
+    @Test
+    public void shouldTurnRoverAtCommandR(){
+        TestRover rover = new TestRover(new Coordinates(3,2), Orientation.S);
+        MarsRover marsRover = new MarsRover(new Plateau(new Coordinates(5,5)));
+        marsRover.sendCommandsToRover("R", rover);
+        TestRover expected = new TestRover(new Coordinates(3,2), Orientation.W);
+        assertEquals(expected, rover);
+    }
 }
