5e95508ea6f64a5579d8ab94ea6c08ddcee228b6
StutiSri <stutisrivastv@gmail.com>
2017-06-05 16:32:46 +0530

    Changed name of TestCase - shouldTurn90DegreeLeftAtLCommandAdded to - roverShouldTurnFromNorthToWestAtLCommand

--- src/test/org/tw/marsrover/MarsRoverTest.java
+++ src/test/org/tw/marsrover/MarsRoverTest.java
@@ -7,9 +7,10 @@ import static org.junit.Assert.assertEquals;
 
 public class MarsRoverTest {
     @Test
-    public void roverShouldTurn90DegreeLeftAtLCommand(){
+    public void roverShouldTurnFromNorthToWestAtLCommand(){
         Rover rover = new Rover(new Coordinates(1, 2), Compass.North);
         rover.turnNintyDegreesLeft();
         assertEquals(Compass.West, rover.getDirection());
     }
+    
 }
